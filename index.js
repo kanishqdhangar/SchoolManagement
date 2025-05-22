a6152c89f174727e220bbdaddddf7c4570cea730
kanishqdhangar <131457610+kanishqdhangar@users.noreply.github.com>
2025-05-23 02:10:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import schoolRoutes from "./routes/schoolRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api", schoolRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
