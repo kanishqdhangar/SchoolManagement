bb50fc07348896514207c2239da874fd7182ed9b
kanishqdhangar <131457610+kanishqdhangar@users.noreply.github.com>
2025-05-23 01:55:21 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
 // db.js
-const mysql = require("mysql2");
-require("dotenv").config();
+import mysql from "mysql2/promise";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connection = mysql.createConnection({
+const sslOptions = process.env.DB_SSL === "true"
+  ? {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_CA_PATH),
+      },
+    }
+  : {};
+
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ...sslOptions,
 });
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database.");
-});
-
-module.exports = connection;
